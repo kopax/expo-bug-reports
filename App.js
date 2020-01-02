fda595e028116dcfa0d14ef90a7ab9717dfe96b9
Dimitri Kopriwa <dka@dka.yeutech.com>
2020-01-03 00:32:50 +0100

    demo install react-native-svg

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Square from './svg';
 
+alert(Square);
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Square />
     </View>
   );
 }
